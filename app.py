c079b7faa3e5b9ced66f178f18b4dcd7d746ac64
Tardcarter <tardcarter@gmail.com>
2021-10-11 14:46:08 -0500

    app.py     Added basic error-checking for requests         If username or email is found in RegistrationRequest, informs user of pending request         If username or email is found in ValidUser, informs user of existing account         Both errors return code 400.
    --Aaron

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-from api.model import db, ValidUser
+from api.model import db, ValidUser, RegistrationRequest
 
 
 app = Flask(__name__, static_folder='ualr-bound/build', static_url_path='')
@@ -44,22 +44,45 @@ def register():
         password = request.json.get("password", None)
         email = request.json.get("email", None)
         access_level = request.json.get("access-level", None)
+        userMatch = False
+        regMatch = False
 
         print(f'\nRecieved Request:\nUsername:{username}\nPassword:{password}\nEmail:{email}\nAccess Level:{access_level}\n')
         
         if username and password and email and access_level:
-            print('\nCreating row in database...\n')
-            regRequest = RegistrationRequest(username=username, password=password, email=email, accessLevel=access_level)
-            db.session.add(regRequest)
-            db.session.commit()
-            print('\nCreated new row.\n')
 
-        return jsonify({
-            "user": username,
-            "pass": password,
-            "email": email,
-            "access_level": access_level,
-            }), 200
+            for users in RegistrationRequest:
+                if username == users.username:
+                    regMatch = True
+            for users in ValidUser:
+                if username == users.username:
+                    userMatch = True
+            for users in RegistrationRequest:
+                if email == users.email:
+                    regMatch = True
+            for users in ValidUser:
+                if email == users.email:
+                    userMatch = True
+
+            if not (userMatch or regMatch):
+                print('\nCreating row in database...\n')
+                regRequest = RegistrationRequest(username=username, password=password, email=email, accessLevel=access_level)
+                db.session.add(regRequest)
+                db.session.commit()
+                print('\nCreated new row.\n')
+
+                return jsonify({
+                    "user": username,
+                    "pass": password,
+                    "email": email,
+                    "access_level": access_level,
+                    }), 200
+            elif userMatch:
+                print('\nA user with that information already exists!')
+                return jsonify({"msg":  "Information already exists among valid users."}), 400
+            else:
+                print('\nA request with that information is awaiting approval.')
+                return jsonify({"msg":  "Information already exists on pending request."}), 400
     #access_token = create_access_token(identity=username)
     #return jsonify(access_token=access_token)
 
